86cb29b0387b4f89c087f9827614290afae35126
hamza <hamzacarew@gmail.com>
2019-09-22 23:47:48 +0100

    Added projects & contact page

--- app/views.py
+++ app/views.py
@@ -1,5 +1,20 @@
 from . import app
+from flask import render_template, make_response
 
 @app.route('/')
 def index():
-    return 'Hello World', 200
+
+    res = make_response(render_template('index.html'), 200)                   # Generate response object and return 200
+    return res
+
+
+@app.route('/projects')
+def projects():
+    res = make_response(render_template('projects.html'), 200)
+    return res
+
+
+@app.route('/contact')
+def contact():
+    res = make_response(render_template('contact.html'), 200)
+    return res
