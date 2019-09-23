a8b44be6ef3ef0b8d5f484b6edf07a1dae883e0b
hamza <hamzacarew@gmail.com>
2019-09-23 16:24:55 +0100

    Added global nav bar

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_nav import Nav
 from Config import Config
 
 app = Flask(__name__)
 Bootstrap(app)
+nav = Nav(app)
 app.config.from_object(Config)
 
-from app import views
+from app import views, navigation
