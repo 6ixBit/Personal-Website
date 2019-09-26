a44be8183aed127bacdc489c2e87530975c8720f
hamza <hamzacarew@gmail.com>
2019-09-26 22:51:59 +0100

    Added 404 & 500 error pages

--- app/navigation.py
+++ app/navigation.py
@@ -1,14 +1,10 @@
 from app import nav
-from flask_nav.elements import Navbar, View, Link, Separator
-
+from flask_nav.elements import Navbar, View
 
 topbar = Navbar('',
     View('Home', 'index'),
     View('Projects', 'projects'),
-    View('Contact', 'contact'),
-    Separator(),
-    Link('', 'https://github.com/6ixBit'), navbar
-
+    View('Contact', 'contact')
 )
 
 nav.register_element('top', topbar)
