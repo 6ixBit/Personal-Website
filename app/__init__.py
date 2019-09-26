cd22ddab0acbed7e68abe35ee1f501b742723d63
hamza <hamzacarew@gmail.com>
2019-09-26 22:14:51 +0100

    Completed email funcitonality

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,16 @@ from flask_bootstrap import Bootstrap
 from flask_nav import Nav
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mail import Mail
 from Config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 Bootstrap(app)
 nav = Nav(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-app.config.from_object(Config)
+mail = Mail(app)
+
 
 from app import views, navigation, forms, models
--- app/forms.py
+++ app/forms.py
@@ -3,8 +3,8 @@ from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length
 
 class Contact_form(FlaskForm):
-    name = StringField(label='Name', validators=[DataRequired('Enter your name')])
-    email = StringField(validators=[DataRequired('Enter your e-mail address'), Email('Please enter a valid e-mail address')])
+    name = StringField(label='Name', validators=[DataRequired('Please Enter your name')])
+    email = StringField(validators=[DataRequired('Please Enter your e-mail address'), Email('Please enter a valid e-mail address')])
     subject = StringField(validators=[DataRequired('Please enter a subject')])
     message = TextAreaField('Message', validators=[DataRequired('You need to enter a message')])
 
