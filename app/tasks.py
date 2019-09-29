61d1f7c2ebd18be3cbd376751f9e37af01406223
hamza <hamzacarew@gmail.com>
2019-09-29 10:57:38 +0100

    Assigned DB update task to RQ

--- app/tasks.py
+++ app/tasks.py
@@ -10,12 +10,11 @@ def insert_db(url):
     req = requests.get(url)
     result = req.json()
 
-    repos = {
-    }
-
-    git = Git()    # Instance to connect to database model class
+    repos = {}
 
     for x in result:
+        git = Git()    # Instance to connect to database model class
+
         # Extract data needed
         repos['name'] = x['name']
         repos['description'] = x['description']
@@ -31,12 +30,66 @@ def insert_db(url):
         git.created_at = repos['created_at']
         git.size = repos['size']
         git.language = repos['language']
-        git.last_updated = repos['last_updated']
         git.repo_url = repos['repo_url']
+        git.last_updated = repos['last_updated']
 
-        # Commit to DB
-        # db.session.add(git)
-        # db.session.commit()
+        # Commit to DB
+        db.session.add(git)
+        db.session.commit()
 
     return repos
 
+def update_db(url):
+
+    req = requests.get(url)         # Make request to GitHub API
+    result = req.json()             # Serve response as JSON
+
+    repos = {}                      # Hold temporary results pulled from Gituhb
+    listy = []                      # List holds info for each repo, represented as dict(s) in a List
+
+    for x in result:
+        # Extract data needed
+        repos['name'] = x['name']
+        repos['description'] = x['description']
+        repos['created_at'] = x['created_at']
+        repos['size'] = x['size']
+        repos['language'] = x['language']
+        repos['last_updated'] = x['updated_at']
+        repos['repo_url'] = x['svn_url']
+        listy.append(repos)
+
+    git = Git.query.all()                                           # Query entire database to be compared with new Git results
+
+    count=0
+
+    for x in git:                                                   # Once each repo has been pulled
+        if x.repo_name == listy[count]['name']:                             # if db val == git api val
+            print(listy[count]['description'])
+            if x.description !=  listy[count]['description']:       # if git val has changed then update db
+                x.description = listy[count]['description']
+                db.session.commit()
+                print(x.repo_name + ' description updated')
+            elif x.created_at != listy[count]['created_at']:
+                x.created_at = listy[count]['created_at']
+                db.session.commit()
+                print(x.repo_name + ' created_at updated')
+            elif  x.size != listy[count]['size']:
+                x.size = listy[count]['size']
+                db.session.commit()
+                print(x.repo_name + ' size updated')
+            elif x.language != listy[count]['language']:
+                x.language = listy[count]['language']
+                db.session.commit()
+                print(x.repo_name + ' language updated')
+            elif x.last_updated != listy[count]['last_updated']:
+                x.last_updated = listy[count]['last_updated']
+                db.session.commit()
+                print(x.repo_name + ' last_updated updated')
+            elif x.repo_url != listy[count]['repo_url']:
+                x.repo_url = listy[count]['repo_url']
+                db.session.commit()
+                print(x.repo_name + ' repo_url updated')
+            else:
+                print('No updates made!')
+            
+        count += 1
